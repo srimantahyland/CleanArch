d3665e9f8db3b4b48e7350c2c4b924dd264b9e84
srimantahyland <srimanta.singh@hyland.com>
2022-02-20 22:22:28 +0530

    Adding Domain Core and Implementation of Memory Bus

---
+++ CleanArch/CleanArch.Domain.Core/Events/Message.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace CleanArch.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
