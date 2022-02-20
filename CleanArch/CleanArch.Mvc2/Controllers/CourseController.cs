a84ce1ad377070fa908846536e3dc10044e3c8a3
srimantahyland <srimanta.singh@hyland.com>
2022-02-20 20:51:04 +0530

    Presentation Layer Changes for Course

---
+++ CleanArch/CleanArch.Mvc2/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Mvc2.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
